7b398e6f864034003cd0d33e870710021c2e99b4
EC2 Default User <ec2-user@ip-172-31-31-145.ec2.internal>
2018-11-12 20:45:23 +0000

    initial block class

---
+++ block.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class block{
+  //private
+  
+  //public
+  public:
+    int IRR;
+    int Recency;
+    int value;
+    
+    //status
+    bool HIR;
+    bool LIR;
+    
+};
