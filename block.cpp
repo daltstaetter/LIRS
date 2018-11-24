888b81459f1684ac625c3341fbe75b45a7bbbc00
Tex Altstaetter <texaltstaetter@tamu.edu>
2018-11-23 21:26:36 -0600

    Fix constructor/deconstructor declaration
    Signed-off-by: Tex Altstaetter <daltstaetter@gmail.com>

--- block.cpp
+++ block.cpp
@@ -8,19 +8,28 @@ class block{
   
   // public
   public:
-    int IRR;
-    int Recency;
-    int value;
-      
+    int IRR;     // Inter-Reference Recency, i.e. the number of unique instructions between two successive calls to same instruction
+    int Recency; // The number of unique instructions have executed since the last call to this instruction
+    // int value;
+    
+    block();  // constructor
+    ~block(); // deconstructor
     // status
     // bool HIR;
     // bool LIR;
     
-    block()
-    {	// Simulates setting initial value to infinity
-	IRR = INT_MAX;
-	Recency = INT_MAX;
-	value = 0;
-
-    }	
 };
+
+
+block::block(void)
+{
+    // Simulates setting initial value to infinity
+    IRR = INT_MAX;
+    Recency = INT_MAX;
+}
+
+block::~block(void)
+{
+    // Deconstructor
+}
+	
